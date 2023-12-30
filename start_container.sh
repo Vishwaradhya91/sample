62383c6ead935d3037535a83235331830c7e244a
Vishwaradhya91 <116479794+Vishwaradhya91@users.noreply.github.com>
2023-12-30 19:13:03 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+##!/bin/bash
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull vishwaradhya97/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 vishwaradhya97/simple-python-flask-app
